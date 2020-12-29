269d0d615821d16414010875e859f5024ff6c98b
Jelle <jelle.becude@student.hu.nl>
2020-12-29 12:33:26 +0100

    iunsertion sort en een list functie gedaan

--- AiSprint 1 (1).py
+++ AiSprint 1 (1).py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 from tkinter import *
-
+import urllib.request
 
 #zet panda library settings
 #globally defined het bronbestand als "steam"
@@ -15,7 +15,32 @@ def file_load():
 file_load()
 
 
-#simpele GUI
+#Zet alles in  een list vanaf een json file url
+def list_functie(thing):
+    mylist = []
+    with urllib.request.urlopen('https://raw.githubusercontent.com/tijmenjoppe/AnalyticalSkills-student/master/project/data/steam.json') as url:
+        data = json.loads(url.read().decode())
+        for i in data:
+            ree = i[thing]
+            mylist.append(ree)
+    print(mylist)
+
+
+# Function to do insertion sort
+def insertionSort(the_list):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(the_list)):
+        key = the_list[i]
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < the_list[j]:
+            the_list[j + 1] = the_list[j]
+            j -= 1
+        the_list[j + 1] = key
+
+    #simpele GUI
 #(voorlopig) alleen functioneel
 #laat de naam van het eerste spel in het bronbestand zien
 def gui():
